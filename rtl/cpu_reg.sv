ce10c13c5ff0478df3428fa3f3577c83e1fce182
Brian Zhuang <bzhuang@eceprog3.ecn.purdue.edu>
2025-05-13 20:01:05 -0400

    cpu reg didn't commit whoops

---
+++ rtl/cpu_reg.sv
@@ -0,0 +1,26 @@
+/*
+    Primary Registers, will be the ones instructions refer to
+*/
+module cpu_reg (
+    input logic [7:0] in
+    input logic [2:0] store,
+    input logic r, w, clk, rst, rEN, wEN,
+    inout logic [7:0] data_bus
+);
+    //Tri-state Buffer
+    assign data_bus = (read_enable) ? registers[store] : 8'bz;
+    assign data_bus = (write_enable) ? in : data_bus; 
+
+    logic [7:0] registers [0:7];
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+            data_bus <= 0;
+            for (int i = 0; i < 8; i++) begin
+                registers[i] <= 8'h00;
+            end
+        end else if (wEN) begin
+            registers[store] <= data_bus;
+            $display("[cpu_reg] loading = %0h into fsm", data_bus);
+        end
+    end
+endmodule
