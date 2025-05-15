bb31714607d5607d18912440831f021fd1fa7f6a
Brian Zhuang <bzhuang@eceprog2.ecn.purdue.edu>
2025-05-15 17:29:22 -0400

    Brushed up fsm, should be "done" going to move on to other parts now

--- rtl/fsm.sv
+++ rtl/fsm.sv
@@ -9,20 +9,29 @@
        o Send relevent information to the ALU
 */
 module fsm #(
-    parameter S0 = 3'b000;
-    parameter S1 = 3'b001;
-    parameter S2 = 3'b010;
-    parameter S3 = 3'b011;
-    parameter S4 = 3'b100;
-    parameter S5 = 3'b101;
-    parameter S6 = 3'b110;
-    parameter S7 = 3'b111;
+    parameter S_START = 8'h00;
+    parameter S_FETCH = 8'h00;
+    parameter S_DECO = 8'h00;
+    parameter S_MATH_DECO = 8'h00;
+    parameter S_STOP = 8'h00;
+    parameter S_LOAD = 8'h00;
+    parameter S_STORE = 8'h00;
+    parameter S_JUMP = 8'h00;
+    parameter S_ALU = 8'h00;
+    parameter S_XOR = 8'h00;
+    parameter S_OR = 8'h00;
+    parameter S_AND = 8'h00;
+    parameter S_MATH = 8'h00;
+    parameter S_EXEC = 8'h00;
+    parameter S_EXEC = 8'h00;
+    parameter S_NEXT = 8'h00;
+    parameter S_FIN = 8'h00;
     
     // parameter MATH = 3'b000;
     parameter XOR = 8'b001_00_000;
     parameter OR = 8'b010_00_000;
     parameter AND = 8'b011_00_000;
-    parameter SAVE = 8'b100_00_000;
+    parameter STOP = 8'b100_00_000;
     parameter LOAD = 8'b101_00_000;
     parameter STORE = 8'b110_00_000;
     parameter JUMP = 8'b111_00_000;
@@ -36,6 +45,7 @@ module fsm #(
     output logic [7:0] addBus, op, 
     output logic flag_zero
 );
+    //Operation Information Conditionals
     always_comb begin
         case(instr)
             8'b001_xx_xxx: op = XOR;
@@ -49,48 +59,43 @@ module fsm #(
         endcase
     end
 
-    logic [3:0] rEN_n, wEN_n, addBus_n; 
     logic [2:0] op_n;
     logic [2:0] state, state_n;
     always_ff @(posedge clk, posedge rst) begin
         if (rst) begin
-            op <= 0;
-            rEN <= 0;
-            wEN <= 0;
-            addBus <= 0;
-            op <= 0;
             state <= INIT;
             flag_zero <= 0;
         end else if (addBus_n == 0) begin
-            op <= op_n;
-            rEN <= rEN_n;
-            wEN <= wEN_n;
-            addBus <= addBus_n;
-            op <= op_n;
             state <= state_n;
             flag_zero <= 1;
         end else begin
-            op <= op_n;
-            rEN <= rEN_n;
-            wEN <= wEN_n;
-            addBus <= addBus_n;
-            op <= op_n;
             state <= state_n;
             flag_zero <= 0;
         end
     end
 
+    //Instructions for ALU 
     always_comb begin
         case(state)
             //FETCH STAGE
-            S0: state_n = S1;
-            S1: state_n = S2;
-            S2: state_n = S3;
-            S3: state_n = S4;
-            S4: state_n = S5;
-            S5: state_n = S6;
-            S6: state_n = S7;
-            S7: state_n = S0;
+            S_START: state_n = S_FETCH;
+            S_FETCH: state_n = S_DECE;
+
+            //DECODE STAGE
+            S_DECO : state_n = (op == XOR) ? S_XOR:
+                               (op == OR) ? S_OR:
+                               (op == AND) ? S_AND:
+                               (op == STOP) ? S_STOP:
+                               (op == LOAD) ? S_LOAD:
+                               (op == STORE) ? S_STORE:
+                               (op == JUMP) ? S_JUMP:
+                               S_MATH;
+            S_MATH: state_n = S_MATH_DECO;
+            S_XOR, S_OR, S_ANDS, S_MATH_DECO: state_n = S_ALU;
+            
+            //EXECUTE STAGE
+            S_ALU, S_STOP, S_LOAD, S_STORE: state_n = S_EXEC;
+            S_EXEC: state_n = S_FIN;
             default: state_n = state;
         endcase
     end
