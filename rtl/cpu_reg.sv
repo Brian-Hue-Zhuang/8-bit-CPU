a43317293403bbb142f9d1edab58d6c66346099a
Brian Zhuang <bzhuang@eceprog2.ecn.purdue.edu>
2025-05-25 21:05:54 -0400

    minor fixes

--- rtl/cpu_reg.sv
+++ rtl/cpu_reg.sv
@@ -4,13 +4,13 @@
 module cpu_reg (
     input logic clk, rst, rEN, wEN,
     input logic [7:0] in
-    input logic [2:0] store,
+    input logic [2:0] select,
     inout tri [7:0] data_bus
 );
     // Tri-state Buffer
-    assign data_bus = (wEN) ? registers[store] : 8'bz;
+    assign data_bus = (wEN) ? registers[select] : 8'bz;
     assign data_bus = (rEN) ? in : data_bus; 
-
+    
     logic [7:0] registers [0:7];
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
@@ -19,7 +19,7 @@ module cpu_reg (
                 registers[i] <= 0;
             end
         end else if (wEN) begin
-            registers[store] <= data_bus;
+            registers[select] <= data_bus;
             $display("[cpu_reg] loading = %0h into fsm", data_bus);
         end
     end
